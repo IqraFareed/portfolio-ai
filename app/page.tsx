47f69d25d27c031676dac90543044f0abbcabfc5
iqra fareed <iqrafareed56@gmail.com>
2024-02-10 13:13:40 +0500

    section divider

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import IntroSection from "@/components/intro-section";
+import SectionDivider from "@/components/section-divider";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <IntroSection />
+      <SectionDivider />
     </main>
   );
 }
